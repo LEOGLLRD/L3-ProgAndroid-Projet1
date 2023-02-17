b4c8fbf56d1e9ba96ea756918e0d04f568e789a1
edouard-capdepon <debigu.edouard@gmail.com>
2023-02-17 15:17:35 +0100

    En cours
    NavBar finit, home page en cours

--- app/src/main/java/com/example/inventairelol/Fragments/InventoryFragment.java
+++ app/src/main/java/com/example/inventairelol/Fragments/InventoryFragment.java
@@ -12,10 +12,10 @@ import com.example.inventairelol.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link NavBar#newInstance} factory method to
+ * Use the {@link InventoryFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NavBar extends Fragment {
+public class InventoryFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class NavBar extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public NavBar() {
+    public InventoryFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class NavBar extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment NavBar.
+     * @return A new instance of fragment InventoryFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static NavBar newInstance(String param1, String param2) {
-        NavBar fragment = new NavBar();
+    public static InventoryFragment newInstance(String param1, String param2) {
+        InventoryFragment fragment = new InventoryFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class NavBar extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_bar, container, false);
+        return inflater.inflate(R.layout.fragment_inventory, container, false);
     }
 }
--- app/src/main/java/com/example/inventairelol/MainActivity.java
+++ app/src/main/java/com/example/inventairelol/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.inventairelol;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
 
+import com.example.inventairelol.Fragments.HomeFragment;
+import com.example.inventairelol.Fragments.InventoryFragment;
+import com.example.inventairelol.Fragments.ProfileFragment;
+import com.example.inventairelol.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home_page);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+
+
+        binding.bottomNavigationView.setOnItemReselectedListener(item -> {
+
+            switch (item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.inventory:
+                    replaceFragment(new InventoryFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+
+            }
+
+        });
+    }
+
+    //methode pour remplacer un fragment, avec en parametre le fragment de destination
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+
     }
 }
